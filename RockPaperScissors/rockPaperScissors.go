465b500e718edbf6229f46dbc1137b75ab0d8a63
razor999920 <raza9999ali@gmail.com>
2023-08-09 22:31:31 -0600

    initial commit for question 2

--- RockPaperScissors/rockPaperScissors.go
+++ RockPaperScissors/rockPaperScissors.go
@@ -1,7 +1,74 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func moveTotal(c string) int {
+	const (
+		Rock     = 1
+		Paper    = 2
+		Scissors = 3
+	)
+
+	switch c {
+	case "X", "A":
+		return Rock
+	case "Y", "B":
+		return Paper
+	case "Z", "C":
+		return Scissors
+	default:
+		return 0
+	}
+}
+
+func getRoundScore(opp, mine string) int {
+	player1 := moveTotal(opp)
+	player2 := moveTotal(mine)
+
+	if player1 == player2 {
+		return 3
+	} else if player2 > player1 {
+		return 6
+	}
+
+	return 0
+}
 
 func main() {
-	fmt.Println("Rock Paper Scissors Game")
+	file, err := os.Open("strategyGuide.txt")
+	if err != nil {
+		fmt.Println("Error opening file: ", err)
+		return
+	}
+	defer file.Close() // return the  file when the function ends
+
+	totalScore := 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		moves := strings.Fields(line)
+		sum := 0
+
+		// Each line has 2 columns
+		// col 1: oppnent's move
+		// col 2: my move
+		sum = moveTotal(moves[1])
+		// Get the score from the round
+		sum += getRoundScore(moves[0], moves[1])
+
+		// Sum the score
+		totalScore += sum
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file: ", err)
+	}
+
+	fmt.Printf("The final score is: %d", totalScore)
 }
