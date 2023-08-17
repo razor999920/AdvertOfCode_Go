7d61400fac19f59ba2decf6a5f6c305141227490
razor999920 <raza9999ali@gmail.com>
2023-08-16 22:33:19 -0600

    adding solution for the example

--- RucksackReorganization/main.go
+++ RucksackReorganization/main.go
@@ -14,16 +14,47 @@ func main() {
 	}
 	defer file.Close()
 
+	// PriorityItems counter
+	priorityItemsTotal := 0
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
 
 		compartment1 := line[:len(line)/2]
-		comompartment2 := line[len(line)/2:]
-		println(compartment1, comompartment2)
+		compartment2 := line[len(line)/2:]
+
+		priorityItemsTotal += getInvalidItem(compartment1, compartment2)
 	}
+
+	fmt.Printf("The sum of the priorities of the listed items types is: %d", priorityItemsTotal)
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Error reading file:", err)
 	}
+}
+
+func getInvalidItem(compartment1, compartment2 string) int {
+	compartmentSet := make(map[rune]struct{})
+
+	for _, key := range compartment1 {
+		compartmentSet[key] = struct{}{}
+	}
+
+	for _, key := range compartment2 {
+		_, exists := compartmentSet[key]
+		if exists {
+			fmt.Printf("%c", key)
+			acsiPosition := int(key)
+
+			if acsiPosition < 97 {
+				return acsiPosition - int('A') + 27
+			}
+
+			return acsiPosition - int('a') + 1
+		}
+
+	}
+
+	return 0
 }
