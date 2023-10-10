f9fb87437d0f7afa8de1aeccd15231a93ad2f73c
razor999920 <raza9999ali@gmail.com>
2023-10-09 22:18:24 -0600

    populating the queue and the map

--- TuningTrouble/TTPartOne.go
+++ TuningTrouble/TTPartOne.go
@@ -15,6 +15,8 @@ func (q *Queue) IsEmpty() bool {
 
 func (q *Queue) Enqueue(element interface{}) {
 	*q = append(*q, element) // add to the end of queue
+
+	fmt.Println(*q)
 }
 
 func (q *Queue) Dequeue() interface{} {
@@ -28,6 +30,48 @@ func (q *Queue) Dequeue() interface{} {
 	return element
 }
 
+func getFirstMarker(signal string) string {
+	var marker string
+
+	var markerQueue Queue
+	markerMap := make(map[rune]int)
+
+	for _, char := range signal {
+		fmt.Printf("%c\n", char)
+
+		_, ok := markerMap[char]
+		if !ok {
+			// Add the single signal to the maps
+			markerQueue.Enqueue(char)
+			markerMap[char] = 1
+		} else {
+			for {
+				signal := markerQueue.Dequeue()
+				// Remove the value from the map as well
+				if s, ok := signal.(rune); ok {
+					delete(markerMap, s)
+				}
+
+				if signal == char {
+					break
+				}
+			}
+		}
+
+		fmt.Println(markerMap)
+
+		if len(markerMap) == 4 {
+			break
+		}
+	}
+
+	for key := range markerMap {
+		marker += string(key)
+	}
+
+	return marker
+}
+
 func main() {
 	file, err := os.Open("TTDemo.txt")
 	if err != nil {
@@ -38,8 +82,8 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		line := scanner.Text()
+		marker := getFirstMarker(scanner.Text())
 
-		fmt.Println(line)
+		fmt.Printf("Before the first start-of-packer is detacted, %c characters need to be processed", marker)
 	}
 }
