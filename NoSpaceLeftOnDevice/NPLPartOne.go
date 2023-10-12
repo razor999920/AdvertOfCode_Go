01e1bb0f4ceb2cb63ad47550e6312c56431361f5
razor999920 <raza9999ali@gmail.com>
2023-10-11 20:52:34 -0600

    initial commit for NSLON

--- NoSpaceLeftOnDevice/NPLPartOne.go
+++ NoSpaceLeftOnDevice/NPLPartOne.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
+func main() {
+	file, err := os.Open("NSLDemo.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	var result string
+
+	fmt.Println("The sum of the total size of directories is:", result)
 }
